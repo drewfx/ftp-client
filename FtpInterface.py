afd1885a8ec8479f0f732a2e83e0ddce776d78ca
drew ruppel <drewruppel@gmail.com>
2017-11-16 17:16:28 -0800

    add basic structure for new class

---
+++ FtpInterface.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# title           :cli.py
+# description     :FTP Interface class to handle requests and descriptions of commands
+# author          :Andrew Ruppel
+# date            :11/26/2017
+# version         :0.1
+# usage           :
+# notes           :external class utilized by cli.py
+# python_version  :2.7.12
+##############################
+
+# Import modules
+
+
+class FtpInterface:
+    def __init__(self):
+        self.commands = 0
+        self.instance = 0
+
+    def build_commands(self):
+        """Help listing of available commands"""
+        prompt = ""
+        return prompt
+
+    def handle_reqest(self):
+        """"""
