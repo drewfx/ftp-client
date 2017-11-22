931ec03e3d329af05a6e06a0fe36e0fe0fabf3ed
drew ruppel <drewruppel@gmail.com>
2017-11-21 17:15:05 -0800

    add constants

--- constants.py
+++ constants.py
@@ -10,4 +10,21 @@
 # python_version  :2.7.12
 ##############################
 
-ACCEPTED_COMMANDS = ('ls', 'get', 'put', 'quit')
+FILE_SEPARATOR = '/'
+
+# Commands
+COMMAND_LS = 'ls'
+COMMAND_GET = 'get'
+COMMAND_PUT = 'put'
+COMMAND_QUIT = 'quit'
+
+# Accepted client commands
+ACCEPTED_COMMANDS = (COMMAND_LS, COMMAND_GET, COMMAND_PUT, COMMAND_QUIT)
+
+# FTP folder attributes
+CLIENT_DOWNLOAD_FOLDER = './_client_downloads'
+CLIENT_UPLOAD_FOLDER = './_client_uploads'
+
+# Buffer Constant for sockets
+HEADER_SIZE = 10
+BUFFER_SIZE = 4096
